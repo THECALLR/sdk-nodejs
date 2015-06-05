9c516149215133c267ab73b33e492129170b1e32
Michael JACQUIN <mjacquin1@gmail.com>
2015-06-05 11:52:01 +0200

    update package

--- src/callr/test.js
+++
@@ -1,9 +0,0 @@
-var Realtime = require('./realtime'),
-    Command = Realtime.Command;
-
-try {
-    console.log( Command(123) );
-} catch ( e ) {
-    console.log( e );
-    console.log(e.code);
-}
