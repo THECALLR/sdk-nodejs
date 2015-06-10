9d4cc378c994ca452cd5b4d7279c3e1027698f3f
Michael JACQUIN <mjacquin1@gmail.com>
2015-06-10 16:24:30 +0200

    Update sample

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -1,40 +1,38 @@
-var thecallr = require('thecallr');
+var callr = require('callr');
 
 try {
-	// initialize instance Thecallr
+	// initialize instance Callr
 	// set your credentials or an Exception will raise
-	var tc = new thecallr.api("login", "password");
+	var api = new callr.api('login', 'password');
 
 	// Basic example
 	// Example to send a SMS
 	// 1. "call" method: each parameter of the method as an argument
-	tc.call("sms.send", "THECALLR", "+33123456789", "Hello, world", {
-		flash_message: false
-	})
-		.success(function(data) {
-			console.log("Response:", data);
-		})
-		.error(function(error) {
-			console.error("\nError:", error.message);
-			console.error("Code:", error.code);
-			console.error("Data:", error.data);
-		});
+
+	var options = {
+		flash_message: false,
+	};
+
+	api.call('sms.send', 'CALLR', '+33123456789', 'Hello, world', options).success(function(data) {
+		console.log('Response:', data);
+	}).error(function(error) {
+		console.error("\nError:", error.message);
+		console.error('Code:', error.code);
+		console.error('Data:', error.data);
+	});
 
 	// 2. "send" method: parameter of the method is an array
-	var my_array = ["THECALLR", "+33123456789", "Hello, world", {
-		"flash_message": false
-	}];
-	tc.send("sms.send", my_array)
-		.success(function(data) {
-			console.log("Response 2:", data);
-		})
-		.error(function(error) {
-			console.error("\nError:", error.message);
-			console.error("Code:", error.code);
-			console.error("Data:", error.data);
-		});
+	var my_array = ['CALLR', '+33123456789', 'Hello, world', options];
+
+	api.send('sms.send', my_array).success(function(data) {
+		console.log('Response 2:', data);
+	}).error(function(error) {
+		console.error("\nError:", error.message);
+		console.error('Code:', error.code);
+		console.error('Data:', error.data);
+	});
 }
 catch (e) {
-	console.error("Fatal error");
+	console.error('Fatal error');
 	console.error(e);
 }
